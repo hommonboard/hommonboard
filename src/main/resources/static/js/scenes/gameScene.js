259fd0852d7d9c53f81fcc4b94709b67c7586cdf
Ivan Kopylov <ivv.kopylov@gmail.com>
2020-06-26 10:20:15 +0300

    Add path finder

--- src/main/resources/static/js/scenes/gameScene.js
+++ src/main/resources/static/js/scenes/gameScene.js
@@ -22,7 +22,8 @@ export default class GameScene extends Phaser.Scene {
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.hero = this.physics.add.sprite(96, 96, "luna");
+        this.hero = this.physics.add.sprite(64, 64, "luna");
+        this.hero.setOrigin(0, 0);
         this.physics.add.collider(this.hero, borderLayer);
         this.physics.add.collider(this.hero, treeLayer);
 
@@ -32,27 +33,14 @@ export default class GameScene extends Phaser.Scene {
         this.showMap();
 
         this.createMarker();
+        this.createPathFinder();
         this.createDebug();
 
-        this.input.on('pointerdown', function (pointer) {
-            this.target.x = pointer.worldX;
-            this.target.y = pointer.worldY;
-
-            this.debug.setValue("pointerDownPosition", "(" + pointer.worldX + "," + pointer.worldY + ")");
-
-            // Move at 200 px/s:
-            this.physics.moveToObject(this.hero, this.target, 200);
-        }, this);
+        this.input.on('pointerup', this.handleClick, this);
 
         this.input.on('pointermove', function (pointer) {
             this.debug.setValue("pointerPosition",  "(" + pointer.worldX + "," + pointer.worldY + ")");
-
-            // Rounds down to nearest tile
-            var pointerTileX = this.map.worldToTileX(pointer.worldX);
-            var pointerTileY = this.map.worldToTileY(pointer.worldY);
-            this.marker.x = this.map.tileToWorldX(pointerTileX);
-            this.marker.y = this.map.tileToWorldY(pointerTileY);
-            this.marker.setVisible(!this.checkCollision(pointerTileX, pointerTileY));
+            this.updateMarker(pointer);
         }, this);
     }
 
@@ -60,6 +48,36 @@ export default class GameScene extends Phaser.Scene {
         this.marker = this.add.graphics();
         this.marker.lineStyle(3, 0xffffff, 1);
         this.marker.strokeRect(0, 0, this.map.tileWidth, this.map.tileHeight);
+    }
+
+    createPathFinder() {
+        this.finder = new EasyStar.js();
+        var grid = [];
+            for(var y = 0; y < this.map.height; y++){
+                var col = [];
+                for(var x = 0; x < this.map.width; x++){
+                    col.push(this.getTileID(x,y));
+                }
+                grid.push(col);
+            }
+        this.finder.setGrid(grid);
+
+        var acceptableTiles = [0];
+        for (let index in this.map.tilesets) {
+            let tileset = this.map.tilesets[index];
+            let properties = tileset.tileProperties;
+
+            for(var i = 0; i < tileset.total; i++){
+                if(!properties.hasOwnProperty(i)) {
+                    acceptableTiles.push(tileset.firstgid + i);
+                    continue;
+                }
+                if(!properties[i].collides) acceptableTiles.push(tileset.firstgid + i);
+                if(properties[i].cost) this.finder.setTileCost(tileset.firstgid + i, properties[i].cost);
+            }
+        }
+
+        this.finder.setAcceptableTiles(acceptableTiles);
     }
 
     createDebug() {
@@ -74,21 +92,18 @@ export default class GameScene extends Phaser.Scene {
     }
 
     update(time, delta) {
-
         this.debug.setValue("heroPosition", "("+ this.hero.x + "," + this.hero.y + ")");
 
-        let distance = Phaser.Math.Distance.Between(this.hero.x, this.hero.y, this.target.x, this.target.y);
-
-        if (this.hero.body.speed > 0)
-        {
-            this.debug.setValue("distance", distance);
-            if (distance < 10)
-            {
-                this.hero.body.reset(this.target.x, this.target.y);
-            }
-        }
-
         this.showMap();
+    }
+
+    updateMarker(pointer) {
+        // Rounds down to nearest tile
+        var pointerTileX = this.map.worldToTileX(pointer.worldX);
+        var pointerTileY = this.map.worldToTileY(pointer.worldY);
+        this.marker.x = this.map.tileToWorldX(pointerTileX);
+        this.marker.y = this.map.tileToWorldY(pointerTileY);
+        this.marker.setVisible(!this.checkCollision(pointerTileX, pointerTileY));
     }
 
     showMap() {
@@ -109,7 +124,58 @@ export default class GameScene extends Phaser.Scene {
     }
 
     checkCollision(x,y) {
+        let isCollision = false;
+        let tile = this.map.getTileAt(x, y, false, 2);
+        isCollision = tile != null ? tile.collides == true : false;
+
+        if (!isCollision) {
+            tile = this.map.getTileAt(x, y, false, 1);
+            isCollision = tile != null ? tile.collides == true : false;
+        }
+
+        return isCollision;
+    };
+
+    getTileID(x,y) {
         var tile = this.map.getTileAt(x, y, false, 2);
-        return tile != null ? tile.collides == true : false;
+        return tile != null ? tile.index : 0;
+    }
+
+    handleClick(pointer) {
+        this.debug.setValue("pointerDownPosition", "(" + pointer.worldX + "," + pointer.worldY + ")");
+        var x = pointer.worldX;
+        var y = pointer.worldY;
+        var toX = Math.floor(x/64);
+        var toY = Math.floor(y/64);
+
+        let isCollision = this.checkCollision(toX, toY);
+        if (isCollision) return;
+
+        var fromX = Math.floor(this.hero.x/64);
+        var fromY = Math.floor(this.hero.y/64);
+
+        this.finder.findPath(fromX, fromY, toX, toY, (path) => {
+            if (path != null) {
+                this.moveCharacter(path);
+            }
+        });
+        this.finder.calculate();
+    };
+
+    moveCharacter(path) {
+        var tweens = [];
+        for(var i = 0; i < path.length-1; i++){
+            var ex = path[i+1].x;
+            var ey = path[i+1].y;
+            tweens.push({
+                targets: this.hero,
+                x: {value: ex*this.map.tileWidth, duration: 200},
+                y: {value: ey*this.map.tileHeight, duration: 200}
+            });
+        }
+
+        this.tweens.timeline({
+            tweens: tweens
+        });
     };
 }
